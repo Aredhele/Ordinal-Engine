5e84eae33fb33c5cf1759bd70b3f38ec71ed389e
Aredhele <vincentstehly@hotmail.fr>
2018-06-21 15:09:04 +0200

    Add socket base empty class

--- Engine/Header/Runtime/Network/Socket/CSocket.hpp
+++ Engine/Header/Runtime/Network/Socket/CSocket.hpp
@@ -1,4 +1,47 @@
-#include <winsock2.h>
+/// Copyright (C) 2018-2019, Ordinal Engine
+/// Vincent STEHLY--CALISTO, vincentstehly@hotmail.fr
+///
+/// This program is free software; you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation; either version 2 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License along
+/// with this program; if not, write to the Free Software Foundation, Inc.,
+/// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+/// \file       CSocket.hpp
+/// \date       21/06/2018
+/// \project    Ordinal Engine
+/// \package    Runtime/Network/Socket
+/// \author     Vincent STEHLY--CALISTO
 
-SOCKET
+#ifndef ORDINAL_ENGINE_C_SOCKET_HPP__
+#define ORDINAL_ENGINE_C_SOCKET_HPP__
+
+/// \namespace ord
+namespace ord
+{
+
+/// \namespace network
+namespace network
+{
+
+/// \brief TODO
+/// \class CSocket
+class CSocket
+{
+public:
+
+};
+
+} // !namespace
+
+} // !namespace
+
+#endif // !ORDINAL_ENGINE_C_SOCKET_HPP__
