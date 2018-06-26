9118aebfd9e4f1f4f50215b29e3e26fa9b04d019
Aredhele <vincentstehly@hotmail.fr>
2018-06-27 01:24:59 +0200

    Update header notice

--- Engine/Header/Runtime/Rendering/CRenderer.hpp
+++ Engine/Header/Runtime/Rendering/CRenderer.hpp
@@ -1,5 +1,6 @@
 /// Copyright (C) 2018-2019, Ordinal Engine
 /// Vincent STEHLY--CALISTO, vincentstehly@hotmail.fr
+/// See https://vincentcalisto.com/ordinal-engine/
 ///
 /// This program is free software; you can redistribute it and/or modify
 /// it under the terms of the GNU General Public License as published by
