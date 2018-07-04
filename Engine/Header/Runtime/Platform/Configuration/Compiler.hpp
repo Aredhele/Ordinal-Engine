7eb228e4fb4dacc2f655dbde136ef70e57215e99
Aredhele <vincentstehly@hotmail.fr>
2018-07-04 02:27:55 +0200

    Update engine configuration file to support many platforms

--- Engine/Header/Runtime/Platform/Configuration/Compiler.hpp
+++ Engine/Header/Runtime/Platform/Configuration/Compiler.hpp
@@ -24,84 +24,167 @@
 #ifndef ORDINAL_ENGINE_COMPILER_HPP__
 #define ORDINAL_ENGINE_COMPILER_HPP__
 
-#if defined(__SSE__)
-#   define ORD_SSE
-#elif defined(__SSE2__)
-#   define ORD_SSE2
-#elif defined(__SSE3__)
-#   define ORD_SSE3
-#elif defined(__AVX__)
-#   define ORD_AVX
-#elif defined(__AVX2__)
-#   define ORD_AVX2
-#endif
+/// \brief Platform settings
+#if defined(_WIN32) || defined(_WIN64)
+#   define ORDINAL_PLATFORM_WINDOWS
+#   define ORDINAL_API
+#   define ORDINAL_EXPORT __declspec(dllexport)
+#   define ORDINAL_IMPORT __declspec(dllimport)
 
-// 32 / 64
-// mvs / mingw
+#elif defined(__linux__) || defined(__unix__)
+#   define ORDINAL_PLATFORM_LINUX
+#   define ORDINAL_API
+#   define ORDINAL_EXPORT
+#   define ORDINAL_IMPORT
 
-/// \brief Cross-compiler settings (GCC | MSVC)
-#if   defined(ORDINAL_USE_GCC)
-#   define ORD_RESTRICT             __restrict__
-#   define ORD_NO_INLINE            __attribute__ ((noinline))
-#   define ORD_ALIGNMENT(ALIGNMENT) __attribute__ ((aligned (ALIGNMENT)))
-#   define ORD_FORCE_INLINE         __attribute__ ((always_inline))
-#   define ORD_DEBUG_BREAK()        __asm__       ("int $03;")
-
-#elif defined(ORDINAL_USE_MSVC)
-#   define ORD_RESTRICT             __restrict
-#   define ORD_NO_INLINE            __declspec    (noinline)
-#   define ORD_ALIGNMENT(ALIGNMENT) __declspec    (align (ALIGNMENT))
-#   define ORD_FORCE_INLINE         __forceinline
-#   define ORD_DEBUG_BREAK()        __debugbreak()
+#elif defined(__APPLE__) && defined(__MACH__)
+#   define ORDINAL_PLATFORM_APPLE
+#   define ORDINAL_API
+#   define ORDINAL_EXPORT
+#   define ORDINAL_IMPORT
 
 #else
-// None
+#   error "Platform not supported."
 
+#endif
+
+/// \brief Detecting the architecture
+#if (defined(_WIN64)      || defined(__amd64__)   || defined(__x86_64__) || \
+     defined(__ia64__)    || defined(__ia64)      || defined(_M_IA64)    || \
+     defined(__itanium__) || defined(__MINGW64__) || defined(_M_AMD64)   || \
+     defined (_M_X64))
+#   define ORDINAL_PLATFORM_X64
+
+#else
+#   define ORDINAL_PLATFORM_X86
+
+#endif
+
+/// \brief Detecting compiler
+#if defined(__BORLANDC__)
+#   define ORDINAL_COMPILER_BORLAND
+
+#elif defined(__clang__)
+#   define ORDINAL_COMPILER_CLANG
+
+#elif defined(__GNUC__) || defined(__GNUG__) || defined(__MINGW32__)
+#   define ORDINAL_COMPILER_GCC
+
+#elif defined(_MSC_VER)
+#   define ORDINAL_COMPILER_MSC
+
+#elif defined(__INTEL_COMPILER) || defined(__ICL)
+#   define ORDINAL_COMPILER_ICL
+
+#else
+#   error "Compiler not supported."
+
+#endif
+
+/// \brief Cross-compiler settings
+#if defined(ORDINAL_COMPILER_BORLAND)
+#   warning "Not all BORDINALland intrisics are available."
+#   define ORDINAL_RESTRICT
+#   define ORDINAL_NO_INLINE
+#   define ORDINAL_ALIGNMENT(ALIGNMENT)
+#   define ORDINAL_FORCE_INLINE
+#   define ORDINAL_DEBUG_BREAK()
+
+#elif defined(ORDINAL_COMPILER_CLANG)
+#   define ORDINAL_RESTRICT             __restrict
+#   define ORDINAL_NO_INLINE            __attribute__((noinline))
+#   define ORDINAL_ALIGNMENT(ALIGNMENT) __attribute__((alloc_align(ALIGNMENT)))
+#   define ORDINAL_FORCE_INLINE         __attribute__((always_inline))
+#   define ORDINAL_DEBUG_BREAK()        __builtin_debugtrap()
+
+#elif defined(ORDINAL_COMPILER_GCC)
+#   define ORDINAL_RESTRICT             __restrict__
+#   define ORDINAL_NO_INLINE            __attribute__ ((noinline))
+#   define ORDINAL_ALIGNMENT(ALIGNMENT) __attribute__ ((aligned (ALIGNMENT)))
+#   define ORDINAL_FORCE_INLINE         __attribute__ ((always_inline))
+#   define ORDINAL_DEBUG_BREAK()        __asm__       ("int $03;")
+
+#elif defined(ORDINAL_COMPILER_MSC)
+#   define ORDINAL_RESTRICT             __restrict
+#   define ORDINAL_NO_INLINE            __declspec    (noinline)
+#   define ORDINAL_ALIGNMENT(ALIGNMENT) __declspec    (align (ALIGNMENT))
+#   define ORDINAL_FORCE_INLINE         __forceinline
+#   define ORDINAL_DEBUG_BREAK()        __debugbreak()
+
+#elif defined(ORDINAL_COMPILER_ICL)
+#   warning "Not all ICL intrisics are available."
+#   define ORDINAL_RESTRICT
+#   define ORDINAL_NO_INLINE
+#   define ORDINAL_ALIGNMENT(ALIGNMENT) __attribute__ ((aligned(ALIGNMENT)))
+#   define ORDINAL_FORCE_INLINE
+#   define ORDINAL_DEBUG_BREAK()
+
+#endif
+
+/// \brief
+#if defined(__SSE__)
+#   define ORDINAL_SSE
+#endif
+
+#if defined(__SSE2__)
+#   define ORDINAL_SSE2
+#endif
+
+#if defined(__SSE3__)
+#   define ORDINAL_SSE3
+#endif
+
+#if defined(__AVX__)
+#   define ORDINAL_AVX
+#endif
+
+#if defined(__AVX2__)
+#   define ORDINAL_AVX2
 #endif
 
 /// \brief C++ standard backward compatibility
 #if defined(__cplusplus)
 #   if    __cplusplus == 201103L
-#       define ORD_CXX11
-#       define ORD_CXX11_CONSTEXPR constexpr
+#       define ORDINAL_CXX11
+#       define ORDINAL_CXX11_CONSTEXPR constexpr
 #   else
-#        define ORD_CXX11_CONSTEXPR
+#        define ORDINAL_CXX11_CONSTEXPR
 #   endif
 
 #   if  __cplusplus == 201402L
-#       define ORD_CXX14
-#       define ORD_CXX14_CONSTEXPR constexpr
+#       define ORDINAL_CXX14
+#       define ORDINAL_CXX14_CONSTEXPR constexpr
 #   else
-#       define ORD_CXX14_CONSTEXPR
+#       define ORDINAL_CXX14_CONSTEXPR
 #   endif
 
 #   if  __cplusplus == 201703L
-#       define ORD_CXX17
-#       define ORD_CXX17_CONSTEXPR constexpr
+#       define ORDINAL_CXX17
+#       define ORDINAL_CXX17_CONSTEXPR constexpr
 #   else
-#       define ORD_CXX17_CONSTEXPR
+#       define ORDINAL_CXX17_CONSTEXPR
 #   endif
 
 #   if __cplusplus >= 201103L
-#       define ORD_FINAL     final
-#       define ORD_NULLPTR   nullptr
-#       define ORD_OVERRIDE  override
-#       define ORD_NOEXCEPT  noexcept
-#       define ORD_CONSTEXPR constexpr
+#       define ORDINAL_FINAL     final
+#       define ORDINAL_NULLPTR   nullptr
+#       define ORDINAL_OVERRIDE  override
+#       define ORDINAL_NOEXCEPT  noexcept
+#       define ORDINAL_CONSTEXPR constexpr
 #   else
 #       warning "Please update your C++ standard."
-#       define ORD_CXX99
-#       define ORD_FINAL
-#       define ORD_NULLPTR NULL
-#       define ORD_OVERRIDE
-#       define ORD_NOEXCEPT
-#       define ORD_CONSTEXPR
+#       define ORDINAL_CXX99
+#       define ORDINAL_FINAL
+#       define ORDINAL_NULLPTR NULL
+#       define ORDINAL_OVERRIDE
+#       define ORDINAL_NOEXCEPT throw();
+#       define ORDINAL_CONSTEXPR
 #   endif
 
 #   if __cplusplus >= 201103L
-#       define ORD_STATIC_ASSERT(X) static_assert(X);
+#       define ORDINAL_STATIC_ASSERT(X) static_assert(X);
 #   else
-#       define ORD_STATIC_ASSERT(X)
+#       define ORDINAL_STATIC_ASSERT(X)
 #   endif
 
 #else
